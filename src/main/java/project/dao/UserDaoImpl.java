316e8c0d1648b2fc36165922879f938c380b04a7
Rachit Sinha <rachitsinha123@gmail.com>
2019-05-20 03:35:12 +0530

    Modify UserDaoImpl to user Hibernate

--- src/main/java/project/dao/UserDaoImpl.java
+++ src/main/java/project/dao/UserDaoImpl.java
@@ -1,76 +1,48 @@
 package project.dao;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 import project.model.User;
 
 public class UserDaoImpl implements UserDao {
-	
+	private SessionFactory sessionFactory;
+
 	List<User> users;
-	
-	public UserDaoImpl() {
-		this.users = new ArrayList<User>();
+
+	public UserDaoImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 
 	public User getUserById(String id) {
-		for(User user: this.users) {
-			if(user.getUid().equalsIgnoreCase(id)) {
-				return user;
-			}
-		}
+		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	public List<User> getAllUsers() {
-		return this.users;
+		return users;
 	}
 
 	public List<User> searchUser(String searchInput) {
-		List<User> results = new ArrayList<User>();
-		for(User user: this.users) {
-			if(user.getUid().toLowerCase().indexOf(searchInput.toLowerCase()) != -1 
-				|| user.getFirstName().toLowerCase().indexOf(searchInput.toLowerCase()) != -1
-				|| user.getLastName().toLowerCase().indexOf(searchInput.toLowerCase()) != -1
-				|| user.getEmail().toLowerCase().indexOf(searchInput.toLowerCase()) != -1
-				|| user.getPhoneNumber().indexOf(searchInput) != -1) {
-				
-				results.add(user);
-			}
-		}
-		return results;
-	}
-
-	public String addUser(User user) {
-		this.users.add(user);
-		return user.getUid();
-	}
-
-	public String deleteUserById(String id) {
-		int index = -1;
-		for(int i=0; i < this.users.size(); i++) {
-			if(this.users.get(i).getUid().equalsIgnoreCase(id)) {
-				index = i;
-				break;
-			}
-		}
-		
-		if(index != -1) {
-			User removed = this.users.remove(index);
-			return removed.getUid();
-		}
-		
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public String updateUserById(String id, User userObj) {
-		for(User user: this.users) {
-			if(user.getUid().equalsIgnoreCase(id)) {
-				user = userObj;
-				user.setUid(id);
-				return user.getUid();
-			}
-		}
+	public String addUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteUserById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateUserById(String id, User user) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
